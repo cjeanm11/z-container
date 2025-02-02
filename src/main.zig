cc32152d9588540e6ceb7ded8c0c0273a5e83d2e
cjeanm <jeanmichelcid@gmail.com>
2025-02-01 19:52:54 -0500

    fix

--- src/main.zig
+++ src/main.zig
@@ -2,25 +2,37 @@ const std = @import("std");
 const syscalls = @import("syscalls.zig");
 
 pub fn main() !void {
-    std.debug.print("Starting Zig container...\n", .{});
+    std.debug.print("> Starting Zig container...\n", .{});
 
     try syscalls.unshare(syscalls.CLONE_NEWPID | syscalls.CLONE_NEWNS | syscalls.CLONE_NEWUTS);
+    std.debug.print("> Unshared namespaces.\n", .{});
+
     try syscalls.sethostname("zig-container");
+    std.debug.print("> Set hostname.\n", .{});
+
     try syscalls.chroot("/home/liz/ubuntufs");
+    std.debug.print("> Changed root to /home/liz/ubuntufs.\n", .{});
+
     var dir = try std.fs.cwd().openDir("/", .{});
     defer dir.close();
     try dir.setAsCwd();
-    
+    std.debug.print("> Set working directory to new root.\n", .{});
+
     try syscalls.create_cgroup("zig-container");
+    std.debug.print("> Created cgroup.\n", .{});
+
     try syscalls.set_cgroup_limit("zig-container", "memory.max", "512M");
     try syscalls.set_cgroup_limit("zig-container", "cpu.max", "50000 100000");
     try syscalls.set_cgroup_limit("zig-container", "pids.max", "20");
     try syscalls.add_process_to_cgroup("zig-container");
+    std.debug.print("> Cgroup limits applied.\n", .{});
 
     try syscalls.mount("proc", "/proc", "proc", 0, null);
-    
+    std.debug.print("> Mounted proc filesystem.\n", .{});
+
     const args: [*]const ?[*:0]const u8 = &[_]?[*:0]const u8{ "/bin/sh", null };
     const envp: [*]const ?[*:0]const u8 = &[_]?[*:0]const u8{ "PATH=/bin", null };
 
+    std.debug.print("> Executing: {s}\n", .{args[0].?});
     try syscalls.execve(args[0].?, args, envp);
 }
